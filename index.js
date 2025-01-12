8f41f300224b0395d353bd81392cdbe942e1c220
DmitryPetrov13 <shyqchqn@gmail.com>
2025-01-12 19:34:49 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// [index.js]
+
+const express = require("express");
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+app.get('/hello-world', (request, response) => {
+  response.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
